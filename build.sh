7d976a36748c6214734a4dec6271e475dc48c6b9
ljcucc <ljcucc@gmail.com>
2022-05-13 02:36:51 +0800

    chnage dist to docs

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-HTML_DIR="./dist/"
-
-rm -rf ./dist/*
+rm -rf ./docs/*
 
 for file in ./md/*; do
   filename=$(basename $file)
-  output=$HTML_DIR${filename/.md/.html}
+  output=./docs/${filename/.md/.html}
   pandoc $file -o $output --template=./src/template.html
 done
 
-cp ./src/index.html ./dist/
-cp ./src/style.css ./dist/
+cp ./src/index.html ./docs/
+cp ./src/style.css ./docs/
